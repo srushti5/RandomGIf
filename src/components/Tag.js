36f74782cf733c22abe08d275c60e8aef32d0883
srushti5 <121479415+srushti5@users.noreply.github.com>
2025-07-13 13:35:34 +0530

    Update Tag.js

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -5,7 +5,7 @@ import Spinner from './Spinner';
 import useGif from '../hooks/useGif';
 
 
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+//const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Tag = () => {
   const [tag, setTag] = useState('car');
